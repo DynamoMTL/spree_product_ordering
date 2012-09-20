e58d7ecb3b62f024b0a6c9d6ea32ae4f9d36e3ce
Ryan Siddle <ryan@whitestarmedia.co.uk>
2012-09-20 12:13:30 +0200

    Don't add column if it's already there

--- db/migrate/20120523141152_add_position_to_product.rb
+++ db/migrate/20120523141152_add_position_to_product.rb
@@ -1,5 +1,7 @@
 class AddPositionToProduct < ActiveRecord::Migration
   def change
-    add_column :spree_products, :position, :integer
+    unless column_exists? :spree_products, :position
+      add_column :spree_products, :position, :integer
+    end
   end
 end
